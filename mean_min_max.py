849f0fcb7efc0020f48c625d52591801871abd7c
Neftin <giammarco.valenti@unitn.it>
2018-11-15 09:45:28 +0100

    Warn about missing data file

--- mean_min_max.py
+++ mean_min_max.py
@@ -1,6 +1,6 @@
 import numpy
 import matplotlib.pyplot
-
+#Fixme provide data file
 data = numpy.loadtxt(fname='inflammation-01.csv', delimiter=',')
 
 fig = matplotlib.pyplot.figure(figsize=(10.0, 3.0))
